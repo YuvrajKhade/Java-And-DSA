e6afed6dd102149fa9dd66ebf4b7322815b2b23c
YuvrajKhade <khadeyuvraj8@gmail.com>
2024-06-11 21:12:20 +0530

    to check the the ith bit of number whether it is 0 or 1

---
+++ dsa_java/Bit_manipulation/ithBit.java
@@ -0,0 +1,17 @@
+package dsa_java.Bit_manipulation;
+
+public class ithBit {
+
+    public static void getIthBit(int num, int i){
+
+        if((num & (1<<i))==0){
+            System.out.println("0");
+        }
+        else{
+            System.out.println("1");
+        }
+    }
+    public static void main(String[] args) {
+        getIthBit(10, 3);
+    }
+}
