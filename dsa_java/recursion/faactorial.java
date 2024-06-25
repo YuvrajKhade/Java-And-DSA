632b188d3b7a4043caae54690d6d05d919fcf95e
YuvrajKhade <khadeyuvraj8@gmail.com>
2024-06-25 20:47:07 +0530

    final commit

--- dsa_java/recursion/faactorial.java
+++ dsa_java/recursion/faactorial.java
@@ -15,6 +15,6 @@ public class faactorial {
         
     }
     public static void main(String[] args) {
-        System.out.println("factorial is: "+fact(6));
+        System.out.println("factorial is: "+fact(-4));
     }
 }
