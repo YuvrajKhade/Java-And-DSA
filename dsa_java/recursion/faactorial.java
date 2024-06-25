6d85239a56d48045dc13d7bc658725fdedc2f3c2
YuvrajKhade <khadeyuvraj8@gmail.com>
2024-06-25 20:38:40 +0530

    factorial calculation

---
+++ dsa_java/recursion/faactorial.java
@@ -0,0 +1,20 @@
+package dsa_java.recursion;
+
+public class faactorial {
+
+    public static int fact(int num){
+        int result=1;
+        if(num<0){
+            return -1;
+        }
+        if(num==0){
+            return 1;
+        }
+        result= num * fact(num-1);
+        return result;
+        
+    }
+    public static void main(String[] args) {
+        System.out.println("factorial is: "+fact(6));
+    }
+}
